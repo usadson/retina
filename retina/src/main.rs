2e21e8da9ebea9d4efd7c5a5dc93f35a468805fa
Tristan Gerritsen <code@thewoosh.org>
2023-06-06 18:07:51 +0200

    [main] Default log level to `warn` on debug builds
    Using `env_logger::init()`, the default log level is only `error`. This
    can be modified by the `RUST_LOG` environment variable, but it is good
    practice for debug builds to display warnings, errors and infos.
    
    This can be modified to silence the logs or extend the logs, whether in
    debug builds or release builds, since `RUST_LOG` always takes
    precedence.

--- retina/src/main.rs
+++ retina/src/main.rs
@@ -6,7 +6,13 @@ mod app;
 use app::Application;
 
 fn main() {
-    env_logger::init();
+    if cfg!(debug_assertions) {
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn"))
+            .target(env_logger::Target::Stdout)
+            .init();
+    } else {
+        env_logger::init();
+    }
 
     let window = retina_gfx::window::Window::new()
         .expect("failed to create window");
