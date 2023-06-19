548cf380eb36e3bbb0586d82e29a633c36b27580
Tristan Gerritsen <code@thewoosh.org>
2023-06-19 21:14:04 +0200

    [compositor] Paint borders of layout nodes
    This change paints the border colors, when they are specified, on layout
    nodes. Edges currently override the other ones, whereas they should be
    painted as triangles in the edges, but it's fine for now!

--- retina-compositor/src/lib.rs
+++ retina-compositor/src/lib.rs
@@ -3,7 +3,8 @@
 
 use retina_gfx::{canvas::CanvasPainter, euclid::{Rect, Point2D, Size2D}};
 use retina_layout::LayoutBox;
-use retina_style::CssColor;
+use retina_style::{CssColor, CssDecimal, CssLineStyle};
+use retina_style_computation::BorderProperties;
 
 pub struct Compositor {
 
@@ -15,18 +16,80 @@ impl Compositor {
         }
     }
 
+    fn calculate_border_rect_bottom(
+        &self,
+        mut position: Point2D<CssDecimal, CssDecimal>,
+        layout_box: &LayoutBox,
+    ) -> Rect<CssDecimal, CssDecimal> {
+        position.y += layout_box.dimensions().height().value() + layout_box.dimensions().border().top().value();
+        let size = Size2D::new(
+            layout_box.dimensions().border().left().value()
+                + layout_box.dimensions().width().value()
+                + layout_box.dimensions().border().right().value(),
+            layout_box.dimensions().border().bottom().value(),
+        );
+        Rect::new(position, size)
+    }
+
+    fn calculate_border_rect_left(
+        &self,
+        position: Point2D<CssDecimal, CssDecimal>,
+        layout_box: &LayoutBox,
+    ) -> Rect<CssDecimal, CssDecimal> {
+        let size = Size2D::new(
+            layout_box.dimensions().border().left().value(),
+            layout_box.dimensions().border().top().value()
+                + layout_box.dimensions().height().value()
+                + layout_box.dimensions().border().bottom().value(),
+        );
+        Rect::new(position, size)
+    }
+
+    fn calculate_border_rect_right(
+        &self,
+        mut position: Point2D<CssDecimal, CssDecimal>,
+        layout_box: &LayoutBox,
+    ) -> Rect<CssDecimal, CssDecimal> {
+        position.x += layout_box.dimensions().width().value() + layout_box.dimensions().border().left().value();
+        let size = Size2D::new(
+            layout_box.dimensions().border().right().value(),
+            layout_box.dimensions().border().top().value()
+                + layout_box.dimensions().height().value()
+                + layout_box.dimensions().border().bottom().value(),
+        );
+        Rect::new(position, size)
+    }
+
+    fn calculate_border_rect_top(
+        &self,
+        position: Point2D<CssDecimal, CssDecimal>,
+        layout_box: &LayoutBox,
+    ) -> Rect<CssDecimal, CssDecimal> {
+        let size = Size2D::new(
+            layout_box.dimensions().border().left().value()
+                + layout_box.dimensions().width().value()
+                + layout_box.dimensions().border().right().value(),
+            layout_box.dimensions().border().top().value(),
+        );
+        Rect::new(position, size)
+    }
+
     pub fn paint(&self, layout_box: &LayoutBox, painter: &mut CanvasPainter) {
         _ = layout_box;
 
-        self.paint_inner(layout_box, painter);
+        self.paint_background(layout_box, painter);
+        self.paint_border(layout_box, painter);
 
         for child in layout_box.children() {
             self.paint(child, painter);
         }
     }
 
-    fn paint_inner(&self, layout_box: &LayoutBox, painter: &mut CanvasPainter) {
-        let position = Point2D::new(0.0, 0.0);
+    fn paint_background(&self, layout_box: &LayoutBox, painter: &mut CanvasPainter) {
+        let position = Point2D::new(
+            layout_box.dimensions().border().left().value(),
+            layout_box.dimensions().border().top().value(),
+        );
 
         let width = layout_box.dimensions().width().value();
         let height = layout_box.dimensions().height().value();
@@ -37,16 +100,60 @@ impl Compositor {
             return;
         }
 
-        let CssColor::Color(background_color) = layout_box.computed_style().background_color() else {
+        match layout_box.computed_style().background_color() {
+            CssColor::Color(background_color) => {
+                if background_color.alpha() <= 0.0 {
+                    return;
+                }
+
+                painter.paint_rect_colored(Rect::new(position, size), background_color);
+            }
+        }
+    }
+
+    fn paint_border(&self, layout_box: &LayoutBox, painter: &mut CanvasPainter) {
+        let position = Point2D::new(
+            0.0,
+            0.0
+        );
+
+        self.paint_border_part(
+            layout_box.computed_style().border_bottom,
+            self.calculate_border_rect_bottom(position, layout_box),
+            painter,
+        );
+
+        self.paint_border_part(
+            layout_box.computed_style().border_left,
+            self.calculate_border_rect_left(position, layout_box),
+            painter,
+        );
+
+        self.paint_border_part(
+            layout_box.computed_style().border_right,
+            self.calculate_border_rect_right(position, layout_box),
+            painter,
+        );
+
+        self.paint_border_part(
+            layout_box.computed_style().border_top,
+            self.calculate_border_rect_top(position, layout_box),
+            painter,
+        );
+    }
+
+    fn paint_border_part(
+        &self,
+        border: BorderProperties,
+        rect: Rect<CssDecimal, CssDecimal>,
+        painter: &mut CanvasPainter,
+    ) {
+        let CssLineStyle::Solid = border.style else {
             return;
         };
 
-        if background_color.alpha() <= 0.0 {
-            return;
+        match border.color {
+            CssColor::Color(color) => painter.paint_rect_colored(rect, color),
         }
-
-        println!("Colored rect: {size:?} @ {position:?} with color {background_color:?}");
-
-        painter.paint_rect_colored(Rect::new(position, size), background_color);
     }
 }
