c9474fb3064b8a7ae3862223afe88093aff6296b
Tristan Gerritsen <code@thewoosh.org>
2023-06-27 12:01:35 +0200

    [style-comp] `font-size` should be inheritable

--- retina-style-computation/src/cascade.rs
+++ retina-style-computation/src/cascade.rs
@@ -33,6 +33,7 @@ fn inherit_property<T>(target: &mut Option<T>, source: &Option<T>)
 
 fn inherit_properties(property_map: &mut PropertyMap, parent: &PropertyMap) {
     inherit_property(&mut property_map.color, &parent.color);
+    inherit_property(&mut property_map.font_size, &parent.font_size);
     // `display` is not inherited
     inherit_property(&mut property_map.white_space, &parent.white_space);
 }
