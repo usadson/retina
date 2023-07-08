a52831b373f6aae6950878b3e160aca73bfae446
Tristan Gerritsen <code@thewoosh.org>
2023-07-08 22:13:54 +0200

    [layout] Add the `LineBoxFragment` type
    The fragments of line boxes are essentially the parts of a bigger text
    block that was cut off because of line wrapping.

--- retina-layout/src/boxes/line.rs
+++ retina-layout/src/boxes/line.rs
@@ -1,6 +1,8 @@
 // Copyright (C) 2023 Tristan Gerritsen <tristan@thewoosh.org>
 // All Rights Reserved.
 
+use euclid::default::{Point2D, Size2D};
+use retina_common::StrTendril;
 use retina_style::CssDecimal;
 
 /// The rectangular area that contains the boxes that form a line is called a
@@ -15,5 +17,24 @@ impl LineBox {
         Self {
             height: Default::default(),
         }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct LineBoxFragment {
+    pub(crate) position: Point2D<CssDecimal>,
+    pub(crate) text: StrTendril,
+    pub(crate) size: Size2D<CssDecimal>,
+}
+
+impl LineBoxFragment {
+    #[inline]
+    pub const fn position(&self) -> Point2D<CssDecimal> {
+        self.position
+    }
+
+    #[inline]
+    pub fn text(&self) -> &str {
+        &self.text
     }
 }
