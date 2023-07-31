1c8f7ac934b26bdd79306d553cd09d7b6fd3105a
Tristan Gerritsen <code@thewoosh.org>
2023-07-31 16:00:30 +0200

    [compositor] Hide most messages behind `trace` log level

--- retina/src/main.rs
+++ retina/src/main.rs
@@ -9,7 +9,7 @@ use retina_gfx::{Window, WindowPainter, WindowSurface};
 
 fn main() {
     if cfg!(debug_assertions) {
-        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn,retina,retina_common,retina_compositor,retina_dom,retina_gfx,retina_gfx_font,retina_layout,retina_page,retina_platform_object,retina_scrittura,retina_style,retina_style_computation,retina_style_parser,retina_user_agent"))
+        env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn,retina,retina_common,retina_compositor=info,retina_dom,retina_gfx,retina_gfx_font,retina_layout,retina_page,retina_platform_object,retina_scrittura,retina_style,retina_style_computation,retina_style_parser,retina_user_agent"))
             .target(env_logger::Target::Stdout)
             .init();
     } else {
