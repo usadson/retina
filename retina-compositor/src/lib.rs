5a1e85132895d54045ebde08e1afd93bfb9555ae
Tristan Gerritsen <code@thewoosh.org>
2023-07-31 13:45:22 +0200

    [compositor] Composite tiles in parallel
    Previous commit introduced the parallelization of the tile rendering,
    but the composition wasn't parallelized. This commit employs the
    synchronized channel approach by allowing the composition of painted
    tiles to be serially rendered when they arrive together in a small
    amount of time.

--- retina-compositor/src/lib.rs
+++ retina-compositor/src/lib.rs
@@ -5,24 +5,30 @@ mod painting;
 mod tile;
 
 use std::{
-    sync::Mutex,
-    time::Instant,
+    sync::{
+        mpsc::{
+            channel,
+            RecvTimeoutError,
+        },
+        Mutex,
+    },
+    time::{
+        Duration,
+        Instant,
+    },
 };
 
 use retina_gfx::{
     Context,
-    euclid::{
-        Point2D,
-        Rect,
-    },
-    Painter,
+    Painter, euclid::Rect,
 };
 use retina_layout::LayoutBox;
+use tile::TileSpace;
 use tracing::instrument;
 
 use self::tile::{
     Tile,
-    TileSpace,
+    tile_rect_by_coordinate,
     TILE_SIZE,
 };
 
@@ -30,6 +36,7 @@ use self::tile::{
 pub struct Compositor {
     context: Context,
     tiles: Vec<Vec<Mutex<Tile>>>,
+    tile_textures: Vec<Vec<wgpu::TextureView>>,
 }
 
 impl Compositor {
@@ -37,6 +44,7 @@ impl Compositor {
         Self {
             context,
             tiles: Vec::new(),
+            tile_textures: Vec::new(),
         }
     }
 
@@ -69,18 +77,19 @@ impl Compositor {
     }
 
     #[instrument(skip_all)]
-    pub async fn paint<Unit, Callback>(
+    pub async fn paint<Callback>(
         &mut self,
         layout_box: &LayoutBox,
-        viewport: Rect<f64, Unit>,
-        tile_ready_callback: &Callback,
+        painter: &mut Painter<'_>,
+        upload_image_callback: Callback,
     )
-            where Callback: Fn(wgpu::TextureView, Rect<u32, TileSpace>) -> () + Sync {
-        let viewport = viewport.cast();
+            where Callback: Fn(&mut Painter<'_>) + Send + Sync {
+        let viewport = painter.viewport_rect().cast();
         let vertical_tiles = divide_and_round_up(viewport.max_y() as _, TILE_SIZE.height);
         let horizontal_tiles = divide_and_round_up(viewport.max_x(), TILE_SIZE.width);
 
         self.tiles.resize_with(vertical_tiles as _, || Vec::with_capacity(horizontal_tiles as _));
+        self.tile_textures.resize_with(vertical_tiles as _, || Vec::with_capacity(horizontal_tiles as _));
 
         let begin = Instant::now();
         log::info!("Preparing tiles: {vertical_tiles} x {horizontal_tiles}");
@@ -89,9 +98,10 @@ impl Compositor {
 
             for x in 0..horizontal_tiles {
                 if row.len() == x as usize {
-                    let point = Point2D::new(x * TILE_SIZE.width, y * TILE_SIZE.height);
-                    let rect = Rect::new(point, TILE_SIZE);
-                    row.push(Mutex::new(Tile::new(self.context.clone(), rect)));
+                    let rect = tile_rect_by_coordinate(x, y);
+                    let tile = Tile::new(self.context.clone(), rect);
+                    self.tile_textures[y as usize].push(tile.canvas.create_view());
+                    row.push(Mutex::new(tile));
                 }
             }
         }
@@ -102,30 +112,86 @@ impl Compositor {
         let viewport_tile_vertical_range = (viewport.min_y() / TILE_SIZE.height)..divide_and_round_up(viewport.max_y(), TILE_SIZE.height);
         let viewport_tile_horizontal_range = (viewport.min_x() / TILE_SIZE.width)..divide_and_round_up(viewport.max_x(), TILE_SIZE.width);
 
+        let (sender, receiver) = channel();
+
+        let mut tile_textures = std::mem::take(&mut self.tile_textures);
+        let tile_textures_ref = &mut tile_textures;
+
         let begin = Instant::now();
         crossbeam::thread::scope(|s| {
+            let viewport_tile_vertical_range2 = viewport_tile_vertical_range.clone();
+            let viewport_tile_horizontal_range2 = viewport_tile_horizontal_range.clone();
+            s.spawn(move |_| {
+                for y in viewport_tile_vertical_range2.clone() {
+                    for x in viewport_tile_horizontal_range2.clone() {
+                        let rect = tile_rect_by_coordinate(x, y).cast();
+                        painter.paint_rect_textured(rect, &tile_textures_ref[y as usize][x as usize]);
+                    }
+                }
+                painter.submit_async_concurrently().wait();
+                upload_image_callback(painter);
+
+                let mut has_new_images = false;
+
+                loop {
+                    match receiver.recv_timeout(Duration::from_millis(50)) {
+                        Ok((view, rect, y, x)) => {
+                            let rect: Rect<u32, TileSpace> = rect;
+                            painter.paint_rect_textured(rect.cast(), &view);
+                            tile_textures_ref[y as usize][x as usize] = view;
+                            has_new_images = true;
+                        }
+
+                        Err(RecvTimeoutError::Timeout) => {
+                            if has_new_images {
+                                painter.submit_async_concurrently().wait();
+                                upload_image_callback(painter);
+                                has_new_images = false;
+                            } else {
+                                log::info!("Still waiting...");
+                            }
+                        }
+
+                        Err(RecvTimeoutError::Disconnected) => {
+                            log::info!("Compositor has no more tiles left!");
+                            return;
+                        }
+                    }
+                }
+            });
+
             for y in viewport_tile_vertical_range.clone() {
                 for x in viewport_tile_horizontal_range.clone() {
                     let tile = &self.tiles[y as usize][x as usize];
+                    let sender = sender.clone();
                     s.spawn(move |_| {
+                        let wait = begin.elapsed().as_millis();
+
                         let mut tile = tile.lock().unwrap();
                         if !tile.dirty {
-                            log::info!("        Tile {y} x {x} cached {} ms", begin.elapsed().as_millis());
+                            log::info!("        Tile {y} x {x} cached {} ms (waited {wait} ms)", begin.elapsed().as_millis());
+                            // TODO: we don't really have to send the tile, no?
+                            _ = sender.send((tile.canvas.create_view(), tile.rect, y, x)).ok();
                             return;
                         }
 
                         tile.paint(layout_box);
-                        log::info!("        Tile {y} x {x} ready in {} ms", begin.elapsed().as_millis());
+                        log::info!("        Tile {y} x {x} ready in {} ms (waited {wait} ms)", begin.elapsed().as_millis());
+
                         if let Some(submission_future) = tile.submission_future.take() {
                             submission_future.wait();
-                            log::info!("        Tile {y} x {x} finished in {} ms", begin.elapsed().as_millis());
+                            log::info!("        Tile {y} x {x} finished in {} ms (waited {wait} ms)", begin.elapsed().as_millis());
                         }
-                        tile_ready_callback(tile.canvas.create_view(), tile.rect);
+
+                        _ = sender.send((tile.canvas.create_view(), tile.rect, y, x)).ok();
                     });
                 }
                 log::info!("    Row {y} done in {} ms", begin.elapsed().as_millis());
             }
+            drop(sender);
         }).unwrap();
+
+        self.tile_textures = tile_textures;
 
         log::info!("Painted in {} ms", begin.elapsed().as_millis());
     }
