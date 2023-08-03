54e59c53ca35e29d99660334f648c043da2a67d6
Tristan Gerritsen <code@thewoosh.org>
2023-08-03 10:58:28 +0200

    [font] Debug FontDescriptor with FontHandle

--- retina-gfx-font/src/handle.rs
+++ retina-gfx-font/src/handle.rs
@@ -26,6 +26,7 @@ impl FontHandle {
 impl Debug for FontHandle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("FontHandle")
+            .field("descriptor", self.descriptor())
             .finish_non_exhaustive()
     }
 }
