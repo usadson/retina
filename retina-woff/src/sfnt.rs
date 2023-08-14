fcc88aa0e8b6631b006e59beb3788612774880a5
Tristan Gerritsen <code@thewoosh.org>
2023-08-14 22:13:21 +0200

    [woff] Remove homegrown woff crate
    For the time being, an already-existing crate is used. When the
    performance or reliability of the other fonts seems to be lacking, it
    might be useful to create our own.

--- retina-woff/src/sfnt.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2023 Tristan Gerritsen <tristan@thewoosh.org>
-// All Rights Reserved.
-
-#[ordinal]
-pub enum SfntVersion {
-    TrueTypeCollection,
-    TrueType,
-    OpenType,
-}
