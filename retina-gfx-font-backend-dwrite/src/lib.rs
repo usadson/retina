9c4b8d9259f987b56e7e6e0fc4f536ddcf135079
Tristan Gerritsen <code@thewoosh.org>
2023-08-16 22:59:59 +0200

    [font-backend-dwrite] Disable on non-Windows platforms

--- retina-gfx-font-backend-dwrite/src/lib.rs
+++ retina-gfx-font-backend-dwrite/src/lib.rs
@@ -1,16 +1,21 @@
 // Copyright (C) 2023 Tristan Gerritsen <tristan@thewoosh.org>
 // All Rights Reserved.
 
+#[cfg(windows)]
 mod color_glyph_run_analysis;
+#[cfg(windows)]
 mod com;
 
+#[cfg(windows)]
 pub use self::color_glyph_run_analysis::ColorGlyphRunAnalysis;
 
+#[cfg(windows)]
 pub struct DWriteWrapper<'font> {
     font: &'font dwrote::Font,
     font_face: &'font dwrote::FontFace,
 }
 
+#[cfg(windows)]
 impl<'font> DWriteWrapper<'font> {
 
     pub fn rasterize(&self) {
