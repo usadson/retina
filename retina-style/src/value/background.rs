356395801b2caefdffe74be873858f576ba8cfc6
Tristan Gerritsen <code@thewoosh.org>
2023-09-10 16:13:28 +0200

    [style] Add `background-position` types

--- retina-style/src/value/background.rs
+++ retina-style/src/value/background.rs
@@ -3,6 +3,42 @@
 
 use strum::{AsRefStr, EnumIter};
 
+use crate::CssLength;
+
+/// # References
+/// * [CSS Backgrounds and Borders Module Level 3](https://drafts.csswg.org/css-backgrounds/#typedef-repeat-style)
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+pub struct CssBackgroundPosition {
+    pub horizontal: CssBackgroundPositionHorizontal,
+    pub vertical: CssBackgroundPositionVertical,
+}
+
+/// # References
+/// * [CSS Backgrounds and Borders Module Level 3](https://drafts.csswg.org/css-backgrounds/#background-position)
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+#[derive(AsRefStr, EnumIter)]
+#[strum(serialize_all="kebab-case")]
+pub enum CssBackgroundPositionHorizontal {
+    #[strum(disabled)]
+    LengthPercentage(CssLength),
+    Left,
+    Center,
+    Right,
+}
+
+/// # References
+/// * [CSS Backgrounds and Borders Module Level 3](https://drafts.csswg.org/css-backgrounds/#background-position)
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd)]
+#[derive(AsRefStr, EnumIter)]
+#[strum(serialize_all="kebab-case")]
+pub enum CssBackgroundPositionVertical {
+    #[strum(disabled)]
+    LengthPercentage(CssLength),
+    Top,
+    Center,
+    Bottom,
+}
+
 /// # References
 /// * [CSS Backgrounds and Borders Module Level 3](https://drafts.csswg.org/css-backgrounds/#typedef-repeat-style)
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
